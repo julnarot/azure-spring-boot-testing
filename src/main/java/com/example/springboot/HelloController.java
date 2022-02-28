30f7c8cf6e03d0eb1fead665e8f24dc370613245
@Julnarot <rauljhonatan.tola@gmail.com>
2022-02-27 20:31:07 -0500

    now can view a template html

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,18 @@
 package com.example.springboot;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class HelloController {
 
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "Word") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
 
 }
